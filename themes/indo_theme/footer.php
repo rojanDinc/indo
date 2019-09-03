4bc207bc31e07f816809c7f3fa4da58b7ac13eca
Rojan Dinc <rojan.dinc@valtech.se>
2019-09-03 13:53:26 +0200

    Add footerlinks and responsivness to footer

--- themes/indo_theme/footer.php
+++ themes/indo_theme/footer.php
@@ -13,7 +13,7 @@
 <footer class="footer">
 	<div class="container">
 		<div class="columns">
-			<div class="column is-one-third ">
+			<div class="column is-one-third is-one-half-mobile ">
 				<h2 class="subtitle is-2 has-text-white m-b-5">KONTAKTA OSS</h2>
 				<h5 class="is-5 has-text-white has-text-weight-bold">Indo AB</h5>
 				<span class="has-text-white">Bollargatan 2</span>
@@ -25,19 +25,23 @@
 				<br />
 				<span class="has-text-white">info@indo.se</span>
 			</div>
-			<div class="column is-one-third m-r-6">
+			<div class="column is-one-third is-one-half-mobile m-r-6">
 				<h2 class="subtitle is-2 has-text-white">OM INDO</h2>
 				<p class="has-text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada
 					fermentum lacus non luctus. Nullam vel placerat metus. Vestibulum aliquam tortor id lacus semper
 					auctor. Donec sapien enim, feugiat quis urna eu.</p>
 			</div>
-			<div class="column is-one-third ">
+			<div class="column is-one-third is-one-half-mobile ">
 				<h2 class="subtitle is-2 has-text-white">LÄNKAR</h2>
 				<?php
-							wp_list_pages(array(
-								'title_li' => '',
-							));
-						?>
+				wp_nav_menu(array(
+					'theme_location'	=> 'primary',
+					'menu_class'        => "footer-menu", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+					'container_class'   => "", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
+					'container_id'      => "", // (string) The ID that is applied to the container.
+					//'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
+				) );
+				?>
 			</div>
 		</div>
 	</div>
